c0f1e8aa9a51c15bf01786df828332a35118b64d
sameckmatheus <sameckmatheuspro@gmail.com>
2023-11-11 21:58:01 -0300

    chore: add angular components setup

---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css'
+})
+export class PerfilComponent {
+
+}
